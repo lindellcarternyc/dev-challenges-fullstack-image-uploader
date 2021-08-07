4db7c13dcfac0724a8c781bc3e91bca0ed73f57a
Lindell Carter <lindellcarternyc@gmail.com>
2021-08-07 18:13:12 -0400

    uses theme and multiple components in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,32 @@
+import { useState } from 'react'
+import { ThemeProvider } from 'styled-components'
+
 import GlobalStyles from './styles/global.styles'
+import { mainTheme } from './styles/theme'
 
 import Page from './layout/Page'
 import Upload from './views/Upload/Upload'
+import Progress from './views/Progess/Progress'
+
+
+type CurrentView
+  = 'Upload'
+  | 'Progress'
 
 const App = (): JSX.Element => {
+  const [currentView, setCurrentView] = useState<CurrentView>('Progress')
+
+  const render = currentView === 'Upload'
+    ? <Upload />
+    : <Progress />
+
   return (
-    <>
+    <ThemeProvider theme={mainTheme}>
       <GlobalStyles />
       <Page>
-        <Upload />
+        {render}
       </Page>
-    </>
+    </ThemeProvider>
   )
 }
 
